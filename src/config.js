84cd658f6c15b6a7b33bbb223d18b374a48f71a7
Robert Sjoblom <robert.sjoblom@gmail.com>
2018-03-05 16:37:37 +0100

    refactoring continues.

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    apiKey: "AIzaSyBxeb5QjCf-J4iv_r2qv-BRZRWGufYkmYw",
+    authDomain: "chaos-burst.firebaseapp.com",
+    databaseURL: "https://chaos-burst.firebaseio.com",
+    projectId: "chaos-burst",
+    storageBucket: "chaos-burst.appspot.com",
+    messagingSenderId: "332063549155"
+};
+
+const ui = {
+    effectDiv: $("#effect"),
+    burstEffect: $("#eff"),
+    prompt: $("#ask"),
+    timeLength: $("#timeLength"),
+    timeDiv: $("#time")
+};
+
+export { config, ui };
--- src/index.js
+++ src/index.js
@@ -1,63 +1,28 @@
-var config = {
-    apiKey: "AIzaSyBxeb5QjCf-J4iv_r2qv-BRZRWGufYkmYw",
-    authDomain: "chaos-burst.firebaseapp.com",
-    databaseURL: "https://chaos-burst.firebaseio.com",
-    projectId: "chaos-burst",
-    storageBucket: "chaos-burst.appspot.com",
-    messagingSenderId: "332063549155"
-};
+import { config, ui } from "./config.js";
+import { storageAvailable } from "./storageAvailable";
 
+function App() {
+    const tables = {
+        timeResults: [],
+        effectResults: []
+    };
+    firebase.initializeApp(config); //eslint-disable-line
+    const db = firebase.database(); //eslint-disable-line
+    
 
-
-firebase.initializeApp(config); //eslint-disable-line
-var db = firebase.database(); //eslint-disable-line
-
-
-
-var tables = {
-    timeResults: [],
-    effectResults: []
-};
-
-function storageAvailable(type) {
-    try {
-        var storage = window[type],
-            x = "__storage_test__";
-        storage.setItem(x, x);
-        storage.removeItem(x);
-        return true;
-    }
-    catch (e) {
-        return e instanceof DOMException && (
-            // everything except Firefox
-            e.code === 22 ||
-            // Firefox
-            e.code === 1014 ||
-            // test name field too, because code might not be present
-            // everything except Firefox
-            e.name === "QuotaExceededError" ||
-            // Firefox
-            e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
-            // acknowledge QuotaExceededError only if there's something already stored
-            storage.length !== 0;
-    }
 }
 
-let ui = {
-    length: $("#length")
-};
 
-
-function initPage(ui, storageBool, tables) {
-    if (storageBool) {
-        //caching exists
-    } else {
-        //caching doesn't exist
-    }
-}
+// function initPage(ui, storageBool, tables) {
+//     if (storageBool) {
+//         //caching exists
+//     } else {
+//         //caching doesn't exist
+//     }
+// }
 
 function getData(db, location, tablesKey) {
-    db.ref(location)
+    db.ref(location) // db.ref("/room2").on("child-added").then(here we update chat) db.ref("/time").off("child-added")
         .once("value")
         .then(snapshot => {
             snapshot.forEach(x => {
@@ -66,18 +31,20 @@ function getData(db, location, tablesKey) {
             });
         })
         .then(function () {
-            
-            ui.length
-                .text(
-                    tables[tablesKey][getRandomInt(tables[tablesKey].length)]
+            ui.length.text(
+                tables[tablesKey][getRandomInt(tables[tablesKey].length)]
                     .toLowerCase()); //eslint-disable-line
         })
-        .then(function() {
+        .then(function () {
             console.log("This happened!");
         });
 }
 
+
+
+
 getData(db, "/time", "timeResults");
+getData(db, "/effect", "effectResults");
 
 // var userId = firebase.auth().currentUser.uid;
 // return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
@@ -94,23 +61,24 @@ getData(db, "/time", "timeResults");
 //     ui.length.text(tables.timeResults[getRandomInt(100)].toLowerCase()); //eslint-disable-line
 // });
 
-$(document).on("click", ui.length, function () { //eslint-disable-line
-    ui.length.text(tables.timeResults[getRandomInt(100)].toLowerCase().replace('"', "")); //eslint-disable-line
+$(document).on("click", ui.length, function () { 
+    ui.length.text(tables.timeResults[getRandomInt(100)].toLowerCase());
 });
 
 // Let's not get confused.
-db.ref("/effect").on("value", snapshot => {
-    snapshot.forEach(x => {
-        tables.effectResults.push(x.val().effect);
-    });
-    $("#eff").text(tables.effectResults[getRandomInt(10000)].replace('"', "") + "."); //eslint-disable-line
-    shutDown();
-});
-$(document).on("click", "#effect", function () { //eslint-disable-line
-    $("#eff").text(tables.effectResults[getRandomInt(10000)].replace('"', "") + "."); //eslint-disable-line
-});
+// db.ref("/effect").on("value", snapshot => {
+//     snapshot.forEach(x => {
+//         tables.effectResults.push(x.val().effect);
+//     });
+//     $("#eff").text(tables.effectResults[getRandomInt(10000)].replace('"', "") + "."); //eslint-disable-line
+//     shutDown();
+// });
+// $(document).on("click", "#effect", function () { //eslint-disable-line
+//     $("#eff").text(tables.effectResults[getRandomInt(10000)].replace('"', "") + "."); //eslint-disable-line
+// });
 
 function getRandomInt(max) {
+    // up to but not including max
     return Math.floor(Math.random() * Math.floor(max));
 }
 
